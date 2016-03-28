50d116db1f3bb1f55fc98fb9a3344310bd104c99
Jim-Holmstroem <jim.holmstroem@gmail.com>
2016-03-28 20:17:11 +0000

    Handles multiple variables

--- main.hs
+++ main.hs
@@ -20,9 +20,15 @@ import Debug.Trace
 -- use TVar?
 -- Gloss.Interface.IO.Simulate instead?
 
+-- TODO even more restrictive parsing
 -- TODO why is there extra newlines after each new line from /dev/ttyUSB0 ? it wasnt there yesterday..
 
-type Readings = [Float]
+
+type Readings = [[Float]]
+
+
+
+
 
 readData :: MVar Readings -> IO ()
 readData readings = do
@@ -31,19 +37,22 @@ readData readings = do
 
     mapM_ handle rows
         where
-            handle row = save v
-                where v = mapM readMaybe $ splitOn "," row
+            handle row = save $ mapM (readMaybe :: String -> Maybe Float) $ splitOn "," row
             save Nothing = return ()
-            save (Just (value:_)) = putStrLn (show value) >> modifyMVar_ readings (return . (value/128:))
-
+            save (Just values@(value:_)) = do
+                modifyMVar_ readings $ \readings-> return $ zipWith (:) values $ enforceDimension values readings
+                    where
+                        enforceDimension left [] = map (const []) left
+                        enforceDimension left right = right
 
 
 draw :: MVar Readings -> IO Picture
 draw readings = do
     readReadings <- readMVar readings
 
-    return $ graph readReadings <> start readReadings
-        where graph readReadings = color white $ line $ zip [-400,-397..] (take 512 readReadings)
+    return $ mconcat $ map renderVariable readReadings
+        where renderVariable readReadingsVariable = graph readReadingsVariable <> start readReadingsVariable
+              graph readReadingsVariable = color white $ line $ zip [-400,-397..] (take 512 readReadingsVariable)
               start [] = mempty
               start xv@(x:_) = translate (-400) x $ color red $ circle lastVariance
                 where lastVariance = sqrt $ (sum $ map (\x->(x-lastMean)^2) lastReadings) / (max 1 lastLength)
@@ -52,13 +61,13 @@ draw readings = do
                       lastReadings = take 8 xv
 
 
-
-
 eventHandler :: Event -> world -> IO world
 eventHandler _ w = return w
 
+
 updateHandler :: Float -> world -> IO world
 updateHandler dt w = return w
+
 
 main :: IO ()
 main = do
