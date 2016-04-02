a9d191483c6ee2f7e9314d65a1952111fa73bc9a
Jim <jim.holmstroem@gmail.com>
2016-04-03 00:51:42 +0200

    Finally fixing the type Expression

--- src/Expression.hs
+++ src/Expression.hs
@@ -2,8 +2,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ExistentialQuantification #-}
 
-module Expression ( Expr(..)
-                , expr
+module Expression ( Expression(..)
+                , Expression_(..)
                 , x0
                 , x1
                 , x2
@@ -26,7 +26,6 @@ module Expression ( Expr(..)
                 , (:/:)
                 , (:$:)
                 , Component(..)
-                , componentId
                 , Constant(..)
                 , Add(..)
                 , Sub(..)
@@ -35,102 +34,108 @@ module Expression ( Expr(..)
                 , Function(..)
 ) where
 
-data Evaluable = forall e . (Expr e) => MkEvaluable e
-expr :: (Expr e) => e -> Evaluable
-expr = MkEvaluable
 
-class Expr e where
+class Expression_ e where
     eval :: e -> [Double] -> Double
+data Expression = forall e. (Expression_ e) => Expression e
+instance Expression_ Expression where
+    eval (Expression e) = eval e
 
 data Component = Component Int
     deriving (Show, Eq)
+component :: Int -> Expression
+component = Expression . Component
 
-x0, x1, x2, x3, x4, x5, x6, x7, x8, x9 :: Component
-x0 = Component 0
-x1 = Component 1
-x2 = Component 2
-x3 = Component 3
-x4 = Component 4
-x5 = Component 5
-x6 = Component 6
-x7 = Component 7
-x8 = Component 8
-x9 = Component 9
-x :: Int -> Component
-x = Component
+x0, x1, x2, x3, x4, x5, x6, x7, x8, x9 :: Expression
+x0 = component 0
+x1 = component 1
+x2 = component 2
+x3 = component 3
+x4 = component 4
+x5 = component 5
+x6 = component 6
+x7 = component 7
+x8 = component 8
+x9 = component 9
+x :: Int -> Expression
+x = component
 
-componentId :: Component -> Int
-componentId (Component n) = n
 
 data Constant = Constant Double
     deriving (Show, Eq)
+constant :: Double -> Expression
+constant = Expression . Constant
+
+-- TODO negate
 
 data Add a b = Add a b
     deriving (Show, Eq)
--- TODO Sub/Neg possible?
+infixl 6 .+
+(.+) :: (Expression_ e, Expression_ e') => e -> e' -> Expression
+a .+ b = Expression $ Add a b
+infixl 6 :+:
+type a :+: b = Add a b -- TODO (:+:) = Add ??
+
 data Sub a b = Sub a b
     deriving (Show, Eq)
-data Mul a b = Mul a b
-    deriving (Show, Eq)
-data Div a b = Div a b
-    deriving (Show, Eq)
-
-data Function a = Function (Double -> Double) a
-
-infixl 6 :+:
-type a :+: b = Add a b
+infixl 6 .-
+(.-) :: (Expression_ e, Expression_ e') => e -> e' -> Expression
+a .- b = Expression $ Sub a b
 infixl 6 :-:
 type a :-: b = Sub a b
+
+data Mul a b = Mul a b
+    deriving (Show, Eq)
+infixl 7 .*
+(.*) :: (Expression_ e, Expression_ e') => e -> e' -> Expression
+a .* b = Expression $ Mul a b
 infixl 7 :*:
-type a :*: b = Mul a b  -- TODO * has lower binding strength than
+type a :*: b = Mul a b
+
+data Div a b = Div a b
+    deriving (Show, Eq)
+infixl 7 ./
+(./) :: (Expression_ e, Expression_ e') => e -> e' -> Expression
+a ./ b = Expression $ Div a b
 infixl 7 :/:
 type a :/: b = Div a b
-infixr 0 :$:
-type f :$: a = a  -- A bit weird but here for completness purposes
 
-
-infixl 6 .+
-a .+ b = Add a b
-infixl 6 .-
-a .- b = Sub a b
-infixl 7 .*
-a .* b = Mul a b
-infixl 7 ./
-a ./ b = Div a b
-
+data Function a = Function (Double -> Double) a
 infixr 0 .$
-f .$ a = Function f a
+(.$) :: (Expression_ e) => (Double -> Double) -> e -> Expression
+f .$ a = Expression $ Function f a
+infixr 0 :$:
+type f :$: a = Function a
 
+-- TODO Remove the repeated code here with something like (*) <$> a <*> b (and make Expression_ applicative or whutever)
 
-instance (Expr a, Expr b) => Expr (Add a b) where
-    eval (Add a b) rows = (+) grabA grabB
-        where grabA = eval a rows
-              grabB = eval b rows
+instance (Expression_ a, Expression_ b) => Expression_ (Add a b) where
+    eval (Add a b) rows = (+) evalA evalB
+        where evalA = eval a rows
+              evalB = eval b rows
 
-instance (Expr a, Expr b) => Expr (Sub a b) where
-    eval (Sub a b) rows = (-) grabA grabB
-        where grabA = eval a rows
-              grabB = eval b rows
+instance (Expression_ a, Expression_ b) => Expression_ (Sub a b) where
+    eval (Sub a b) rows = (-) evalA evalB
+        where evalA = eval a rows
+              evalB = eval b rows
 
-instance (Expr a, Expr b) => Expr (Mul a b) where
-    eval (Mul a b) rows = (*) grabA grabB
-        where grabA = eval a rows  -- TODO Remove the repeated code here with something like (*) <$> a b (and make Expr applicative or whutever)
-              grabB = eval b rows
+instance (Expression_ a, Expression_ b) => Expression_ (Mul a b) where
+    eval (Mul a b) rows = (*) evalA evalB
+        where evalA = eval a rows
+              evalB = eval b rows
 
-instance (Expr a, Expr b) => Expr (Div a b) where
-    eval (Div a b) rows = (/) grabA grabB
-        where grabA = eval a rows
-              grabB = eval b rows
+instance (Expression_ a, Expression_ b) => Expression_ (Div a b) where
+    eval (Div a b) rows = (/) evalA evalB
+        where evalA = eval a rows
+              evalB = eval b rows
 
-instance (Expr a) => Expr (Function a) where
+instance (Expression_ a) => Expression_ (Function a) where
     eval (Function f a) rows = f $ eval a rows
 
-instance Expr Constant where
-    eval (Constant f) _ = f
-instance Expr Double where
-    eval f _ = f
+instance Expression_ Constant where
+    eval (Constant f) = const f
+instance Expression_ Double where
+    eval f = const f
 
-instance Expr Component where
+instance Expression_ Component where
     eval (Component n) rows = rows !! n
-
-
